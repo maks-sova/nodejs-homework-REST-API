f040a14f2d563f979a06c5c05d539a67b3a4c249
maks-sova <maxxxhill2k@gmail.com>
2023-04-10 20:25:21 +0300

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Home template message' })
+  res.json({ message: 'Ho template message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
